9046cf7aa0f4aa2304891d81ad39a6731e503051
Cihan Kilicarslan <cihankilicarslan@hotmail.com>
2023-08-21 13:11:58 -0400

    first test added rest controller

---
+++ src/main/java/com/keral/inventoryManagementSystem/controller/ReportController.java
@@ -0,0 +1,47 @@
+package com.keral.inventoryManagementSystem.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@RestController
+@RequestMapping("/reports")
+public class ReportController {
+
+    @Autowired
+    private ReportService reportService;
+
+    @GetMapping(value = "/generate", produces = MediaType.APPLICATION_PDF_VALUE)
+    public void generateReport(HttpServletResponse response) {
+        try {
+            byte[] reportBytes = reportService.generateReport();
+
+            response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+            response.setHeader("Content-Disposition", "inline; filename=report.pdf");
+            response.getOutputStream().write(reportBytes);
+        } catch (IOException | JRException | SQLException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping(value = "/generate-sales", produces = MediaType.APPLICATION_PDF_VALUE)
+    public void generateSalesReport(HttpServletResponse response) {
+        try {
+            byte[] reportBytes = reportService.generateReportSale();
+
+            response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+            response.setHeader("Content-Disposition", "inline; filename=sales_report.pdf");
+            response.getOutputStream().write(reportBytes);
+        } catch (IOException | JRException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/keral/inventoryManagementSystem/service/ReportService.java
@@ -0,0 +1,60 @@
+package com.keral.inventoryManagementSystem.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class ReportService {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    public byte[] generateReport() throws JRException, SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            InputStream jasperStream = resourceLoader.getResource("classpath:products_report.jrxml").getInputStream();
+            JasperReport jasperReport = JasperCompileManager.compileReport(jasperStream);
+
+            Map<String, Object> parameters = new HashMap<>();
+            // Add parameters if needed
+            // parameters.put("paramName", paramValue);
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, connection);
+
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public byte[] generateReportSale() throws JRException, SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            InputStream jasperStream = resourceLoader.getResource("classpath:sales_report.jrxml").getInputStream();
+            JasperReport jasperReport = JasperCompileManager.compileReport(jasperStream);
+
+            Map<String, Object> parameters = new HashMap<>();
+            // Add parameters if needed
+            // parameters.put("paramName", paramValue);
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, connection);
+
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
